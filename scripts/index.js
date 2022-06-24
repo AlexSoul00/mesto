5b8cf1d5c03f450ed1d0fa0ec64d82b3218cdf89
Alex Agapov <AlexSoul00@yandex.ru>
2022-06-24 12:48:12 +0300

    feat initial rendering + delete

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,94 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 const editButton = document.querySelector('.profile-info__edit-button')
 const popup = document.querySelector('.popup')
 const closePopupButton = document.querySelector('.popup__close-button')
 const usernameFieldElement = document.querySelector('.popup__input_username')
 const usernameProfileElement = document.querySelector('.profile-info__username')
+// const cardNameFieldElement = 
+// const cardLinkFieldElement = 
 const bioFieldElement = document.querySelector('.popup__input_bio')
 const bioProfileElement = document.querySelector('.profile-info__bio')
 const formElement = document.querySelector('.popup__form')
+const addButton = document.querySelector('.profile__add-button')
+const popupTitleElement = document.querySelector('.popup__title')
+const popupSubmButtonElement = document.querySelector('.popup__subm-button')
+const cardsTemplateElement = document.querySelector('.cards__template')
+const cardsListElement = document.querySelector('.cards');
+const getCardByEvent = e => e.currentTarget.closest('.cards__card');
+
+const createCard = (nameValue, linkValue) => {
+  const card = cardsTemplateElement.content
+  .querySelector('.cards__card')
+  .cloneNode(true);
+  card.querySelector('.cards__text').textContent = nameValue;
+  card.querySelector('.cards__photo').src = linkValue;
+  card.querySelector('.cards__button-delete').addEventListener('click', deleteCard);
+  card.querySelector('.cards__button-like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('cards__button-like_aktive')
+  });
+  return card;
+};
+
+const addCard = (name, link) => {
+  const card = createCard(name, link)
+  cardsListElement.prepend(card);
+};
+
+const deleteCard = e => {
+  const card = getCardByEvent(e);
+
+  card.remove();
+};
+
+initialCards.forEach(card => addCard(card.name, card.link));
+
 
 function openPopup(popupElement) {
   popupElement.classList.add('popup_aktive')
-}
+};
 
 function closePopup(popupElement) {
   popupElement.classList.remove('popup_aktive')
-}
+};
 
 editButton.addEventListener('click', function() {
   openPopup (popup)
+  popupTitleElement.textContent =  'Редактировать профиль';
+  popupSubmButtonElement.textContent = 'Сохранить';
   usernameFieldElement.value = usernameProfileElement.textContent
   bioFieldElement.value = bioProfileElement.textContent
+})
+
+addButton.addEventListener('click', function() {
+  openPopup (popup)
+  popupTitleElement.textContent = 'Новое место';
+  popupSubmButtonElement.textContent = 'Создать';
+  document.querySelector('.popup__input').Placeholder = 'test';
 })
 
 closePopupButton.addEventListener('click', function() {
@@ -27,9 +97,17 @@ closePopupButton.addEventListener('click', function() {
 
 formElement.addEventListener('submit', function(event) {
   event.preventDefault()
+  if (popupTitleElement.textContent === 'Редактировать профиль'){
   usernameProfileElement.textContent = usernameFieldElement.value
   bioProfileElement.textContent = bioFieldElement.value
   closePopup (popup)
-})
-
-
+  }
+  else {
+    const nameValue = usernameFieldElement.value
+    const linkValue = bioFieldElement.value 
+    addCard(nameValue, linkValue);
+    closePopup(popup);
+    formElement.reset();
+  }
+  }
+)
